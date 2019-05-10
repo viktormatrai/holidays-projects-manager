0167a2c516a3bb56c0e3d4576badc9e0275467ad
Viktor Matrai <viktor0matrai@gmail.com>
2019-05-10 07:21:05 +0200

    to hungarian

--- resources/views/example/table.blade.php
+++ resources/views/example/table.blade.php
@@ -6,9 +6,6 @@
                 <tr>
                     <th scope="col">Név</th>
                     <th scope="col">Email</th>
-                    <th scope="col">Felhasználható szabadnapok</th>
-                    <th scope="col">Felhasznált szabadnapok</th>
-                    <th scope="col">Projects working on</th>
                 </tr>
             </thead>
             <tbody>
@@ -16,9 +13,8 @@
                     <tr>
                         <td>{{ $user -> name }}</td>
                         <td>{{ $user -> email }}</td>
-                        <td>{{ $user -> remaining_free_days }}</td>
-                        <td>{{ $user -> used_free_days }}</td>
-                        <td><a class="btn btn-info" href="{{ URL::to("projects") }}">Projects</a></td>
+                        <td><a class="btn btn-info" href="{{ URL::to("projects") }}">aktuális munkák</a></td>
+                        <td><a class="btn btn-info" href="{{ URL::to("projects") }}">Szabadságok</a></td>
                     </tr>
                 @endforeach
             </tbody>
--- resources/views/main/topbar.blade.php
+++ resources/views/main/topbar.blade.php
@@ -1,8 +1,7 @@
 <nav class="navbar navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="/img/money.svg" width="30" height="30" class="d-inline-block align-top mr-2" alt="">
-            Holidays
+            Holiday and project manager
         </a>
     </div>
 </nav>
